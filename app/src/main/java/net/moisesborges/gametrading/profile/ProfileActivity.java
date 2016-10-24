243be1ca1fbc3266f917eb2bee16b8abee2cabd7
moises.anjos <moises.anjos@ibge.gov.br>
2016-10-24 08:39:10 -0200

    fix .gitignore file

---
+++ app/src/main/java/net/moisesborges/gametrading/profile/ProfileActivity.java
@@ -0,0 +1,20 @@
+package net.moisesborges.gametrading.profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import net.moisesborges.gametrading.R;
+
+/**
+ * Created by moises.anjos on 31/08/2016.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
